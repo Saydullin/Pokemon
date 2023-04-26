1bc5798fa77ae54585481126b0a992636c0dac0e
saydullin <saydullindev@gmail.com>
2023-04-26 23:25:27 +0300

    PokemonInfoActivity added

---
+++ app/src/main/java/com/saydullin/pokemon/app/activities/PokemonInfoActivity.kt
@@ -0,0 +1,57 @@
+package com.saydullin.pokemon.app.activities
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModelProvider
+import com.saydullin.pokemon.app.App
+import com.saydullin.pokemon.app.screens.PokemonInfoScreen
+import com.saydullin.pokemon.app.viewmodels.PokemonViewModel
+import com.saydullin.pokemon.app.viewmodels.PokemonViewModelFactory
+import com.saydullin.pokemon.app.ui.theme.PokemonTheme
+import javax.inject.Inject
+
+class PokemonInfoActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var vmFactory: PokemonViewModelFactory
+    private lateinit var vmPokemon: PokemonViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        (applicationContext as App).appComponent.inject(this)
+
+        vmPokemon = ViewModelProvider(this, vmFactory)[PokemonViewModel::class.java]
+
+        val pokemonUrl = intent.getStringExtra("pokemon_url")
+
+        if (pokemonUrl != null) {
+            vmPokemon.getPokemonInfo(pokemonUrl)
+        }
+
+
+        vmPokemon.error.observe(this) {
+            if (it != null) {
+                Toast.makeText(this, "Error $it", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        setContent {
+            PokemonTheme {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    PokemonInfoScreen(vmPokemon)
+                }
+            }
+        }
+    }
+}
+
+
+
