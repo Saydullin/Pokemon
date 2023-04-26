336f01e81d8b8124b314c62d1e145c349d26014a
saydullin <saydullindev@gmail.com>
2023-04-26 23:15:06 +0300

    theme configured

---
+++ app/src/main/java/com/saydullin/pokemon/app/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.saydullin.pokemon.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFFE1BC00)
+val PrimaryDark = Color(0xFFAC8F00)
+val TextOnPrimary = Color(0xFFFFFFFF)
+val Secondary = Color(0xFF0767DD)
+val Tertiary = Color(0xFFA407DD)
+val WindowBackground = Color(0xFFE1BC00)
--- app/src/main/java/com/saydullin/pokemon/app/ui/theme/Type.kt
+++ app/src/main/java/com/saydullin/pokemon/app/ui/theme/Type.kt
@@ -1,34 +1,40 @@
-package com.saydullin.pokemon.ui.theme
+package com.saydullin.pokemon.app.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.saydullin.pokemon.R
 
-// Set of Material typography styles to start with
+val fonts = FontFamily(
+    Font(R.font.roboto_bold, weight = FontWeight.Bold),
+    Font(R.font.roboto_medium, weight = FontWeight.Medium),
+    Font(R.font.roboto_regular, weight = FontWeight.Normal),
+    Font(R.font.roboto_black)
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = fonts,
+        fontWeight = FontWeight.Medium,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+        fontSize = 18.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
