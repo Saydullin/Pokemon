76aaff3cfc310cbb2f026ab2a67d104c142df269
saydullin <saydullindev@gmail.com>
2023-04-26 23:22:12 +0300

    redundant query removed

--- app/src/main/java/com/saydullin/pokemon/api/services/GetPokemonsService.kt
+++ app/src/main/java/com/saydullin/pokemon/api/services/GetPokemonsService.kt
@@ -3,14 +3,10 @@ package com.saydullin.pokemon.api.services
 import com.saydullin.pokemon.domain.models.PokemonBody
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Path
 
 interface GetPokemonsService {
 
     @GET("pokemon")
     suspend fun getPokemons(): Response<PokemonBody>
 
-    // TODO Change result model
-    @GET("pokemon/{id}")
-    suspend fun getPokemonById(@Path("id") id: String): PokemonBody
 }
