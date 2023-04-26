a6f45eafee6170bf2e519ee74ff2f9dc9d04fd84
saydullin <saydullindev@gmail.com>
2023-04-26 23:40:50 +0300

    MainActivity code fixed

--- app/src/main/java/com/saydullin/pokemon/app/activities/MainActivity.kt
+++ app/src/main/java/com/saydullin/pokemon/app/activities/MainActivity.kt
@@ -1,18 +1,18 @@
 package com.saydullin.pokemon.app.activities
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModelProvider
 import com.saydullin.pokemon.app.App
+import com.saydullin.pokemon.app.screens.MainScreen
 import com.saydullin.pokemon.app.viewmodels.PokemonViewModel
 import com.saydullin.pokemon.app.viewmodels.PokemonViewModelFactory
+import com.saydullin.pokemon.app.ui.theme.PokemonTheme
 import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
@@ -26,14 +26,9 @@ class MainActivity : ComponentActivity() {
 
         (applicationContext as App).appComponent.inject(this)
 
-        vmPokemon = ViewModelProvider(this, vmFactory)
-            .get(PokemonViewModel::class.java)
+        vmPokemon = ViewModelProvider(this, vmFactory)[PokemonViewModel::class.java]
 
-        vmPokemon.check()
-
-        vmPokemon.pokemon.observe(this) {
-            Log.e("sady", "got pokemons list! it has ${it.size} size")
-        }
+        vmPokemon.getPokemons()
 
         vmPokemon.error.observe(this) {
             if (it != null) {
@@ -42,13 +37,14 @@ class MainActivity : ComponentActivity() {
         }
 
         setContent {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-            ) {
-                Text(text = "Hello Saydullin")
+            PokemonTheme {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    MainScreen(vmPokemon)
+                }
             }
         }
-
     }
 }
 
