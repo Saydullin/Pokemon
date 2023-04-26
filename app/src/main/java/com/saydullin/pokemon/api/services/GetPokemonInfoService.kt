fcae0fb5bec56a492b349bd249680c6bfd202af7
saydullin <saydullindev@gmail.com>
2023-04-26 23:21:00 +0300

    GetPokemonInfoService added

---
+++ app/src/main/java/com/saydullin/pokemon/api/services/GetPokemonInfoService.kt
@@ -0,0 +1,13 @@
+package com.saydullin.pokemon.api.services
+
+import com.saydullin.pokemon.domain.models.PokemonInfo
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Url
+
+interface GetPokemonInfoService {
+
+    @GET
+    suspend fun getPokemonInfo(@Url url: String): Response<PokemonInfo>
+
+}
