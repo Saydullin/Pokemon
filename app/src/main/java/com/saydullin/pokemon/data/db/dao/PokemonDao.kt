baed40df586afb4317122774c9aa535f438bcd3b
saydullin <saydullindev@gmail.com>
2023-04-26 23:34:36 +0300

    PokemonDao save DB method added

--- app/src/main/java/com/saydullin/pokemon/data/db/dao/PokemonDao.kt
+++ app/src/main/java/com/saydullin/pokemon/data/db/dao/PokemonDao.kt
@@ -1,13 +1,20 @@
 package com.saydullin.pokemon.data.db.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.saydullin.pokemon.data.db.entities.PokemonEntity
+import com.saydullin.pokemon.data.db.entities.PokemonBodyEntity
 
 @Dao
 interface PokemonDao {
 
-    @Query("SELECT * FROM PokemonEntity")
-    fun getPokemons(): List<PokemonEntity>
+    @Query("SELECT * FROM PokemonBodyEntity")
+    fun getPokemonsBody(): PokemonBodyEntity?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun savePokemonsBody(pokemonBodyEntity: PokemonBodyEntity)
 
 }
+
+
