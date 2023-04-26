ea4ddd04a77ed40599b36944a770575fa7750206
saydullin <saydullindev@gmail.com>
2023-04-26 23:46:34 +0300

    Dagger modules added & PokemonInfoActivity injected

--- app/src/main/java/com/saydullin/pokemon/app/di/AppComponent.kt
+++ app/src/main/java/com/saydullin/pokemon/app/di/AppComponent.kt
@@ -1,17 +1,22 @@
 package com.saydullin.pokemon.app.di
 
 import com.saydullin.pokemon.app.activities.MainActivity
+import com.saydullin.pokemon.app.activities.PokemonInfoActivity
 import dagger.Component
 
 @Component(
     modules = [
         NetworkModule::class,
         AppModule::class,
+        DataModule::class,
+        DomainModule::class,
     ]
 )
 interface AppComponent {
 
     fun inject(mainActivity: MainActivity)
+
+    fun inject(pokemonInfoActivity: PokemonInfoActivity)
 
 }
 
