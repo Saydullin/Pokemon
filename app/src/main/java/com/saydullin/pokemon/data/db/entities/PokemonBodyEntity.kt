4415ad8cec8c7f4a1741900d7de3188e3b234d5e
saydullin <saydullindev@gmail.com>
2023-04-26 23:35:39 +0300

    PokemonBodyEntity method typo fixed

--- app/src/main/java/com/saydullin/pokemon/data/db/entities/PokemonBodyEntity.kt
+++ app/src/main/java/com/saydullin/pokemon/data/db/entities/PokemonBodyEntity.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverters
-import com.saydullin.pokemon.data.db.converters.PokemonTypeListConverter
+import com.saydullin.pokemon.data.db.converters.PokemonListConverter
 import com.saydullin.pokemon.domain.models.Pokemon
 import com.saydullin.pokemon.domain.models.PokemonBody
 
@@ -14,10 +14,10 @@ data class PokemonBodyEntity(
     @ColumnInfo val count: Int,
     @ColumnInfo val next: String,
     @ColumnInfo val previous: String,
-    @TypeConverters(PokemonTypeListConverter::class) val pokemonsList: List<Pokemon>,
+    @TypeConverters(PokemonListConverter::class) val pokemonsList: List<Pokemon>,
 ) {
 
-    fun toPokemonBoy() = PokemonBody(
+    fun toPokemonBody() = PokemonBody(
         count = count,
         next = next,
         previous = previous,
