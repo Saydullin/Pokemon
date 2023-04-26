9c7b1e66c5d2722f364f8590ac7cc1df9135bae5
saydullin <saydullindev@gmail.com>
2023-04-26 23:23:22 +0300

    PokemonInfoRepositoryImpl added

---
+++ app/src/main/java/com/saydullin/pokemon/data/repositories/PokemonInfoRepositoryImpl.kt
@@ -0,0 +1,50 @@
+package com.saydullin.pokemon.data.repositories
+
+import android.util.Log
+import com.saydullin.pokemon.api.services.GetPokemonInfoService
+import com.saydullin.pokemon.data.db.dao.PokemonInfoDao
+import com.saydullin.pokemon.domain.models.PokemonInfo
+import com.saydullin.pokemon.domain.repositories.PokemonInfoRepository
+import com.saydullin.pokemon.domain.utils.Resource
+import com.saydullin.pokemon.domain.utils.StatusCode
+import javax.inject.Inject
+
+class PokemonInfoRepositoryImpl @Inject constructor(
+    private val pokemonInfoService: GetPokemonInfoService,
+    private val pokemonInfoDao: PokemonInfoDao
+) : PokemonInfoRepository {
+
+    override suspend fun getPokemonInfoAPI(url: String): Resource<PokemonInfo> {
+        try {
+            val pokemons = pokemonInfoService.getPokemonInfo(url)
+            val pokemonsBody = pokemons.body()
+            Log.e("sady", "pokemonsBody $pokemonsBody")
+            if (pokemons.isSuccessful && pokemonsBody != null) {
+                return Resource.Success(pokemonsBody)
+            }
+
+            return Resource.Error(
+                statusCode = StatusCode.CONNECTION_ERROR
+            )
+        } catch (e: Exception) {
+            return Resource.Error(
+                statusCode = StatusCode.CONNECTION_ERROR
+            )
+        }
+
+    }
+
+    override suspend fun getPokemonInfoDB(url: String): Resource<PokemonInfo> {
+        val pokemon = pokemonInfoDao.getPokemonInfoByUrl(url)
+            ?: return Resource.Error(StatusCode.DATA_NOT_FOUND)
+
+        return Resource.Success(pokemon.toPokemonInfo())
+    }
+
+    override suspend fun savePokemonInfoDB(pokemonInfo: PokemonInfo, url: String) {
+        pokemonInfoDao.savePokemonInfo(pokemonInfo.toPokemonInfoEntity(url))
+    }
+
+}
+
+
---
+++ app/src/main/java/com/saydullin/pokemon/domain/repositories/PokemonInfoRepository.kt
@@ -0,0 +1,16 @@
+package com.saydullin.pokemon.domain.repositories
+
+import com.saydullin.pokemon.domain.models.PokemonInfo
+import com.saydullin.pokemon.domain.utils.Resource
+
+interface PokemonInfoRepository {
+
+    suspend fun getPokemonInfoAPI(url: String): Resource<PokemonInfo>
+
+    suspend fun getPokemonInfoDB(url: String): Resource<PokemonInfo>
+
+    suspend fun savePokemonInfoDB(pokemonInfo: PokemonInfo, url: String)
+
+}
+
+
