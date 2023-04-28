7f34c203cc16f6701258cb14c2bc2bd8dd3eb77f
saydullin <saydullindev@gmail.com>
2023-04-28 14:33:28 +0300

    PokemonList.kt loading progressBar added

--- app/src/main/java/com/saydullin/pokemon/app/screens/PokemonList.kt
+++ app/src/main/java/com/saydullin/pokemon/app/screens/PokemonList.kt
@@ -1,14 +1,18 @@
 package com.saydullin.pokemon.app.screens
 
 import android.content.Intent
-import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.saydullin.pokemon.app.activities.PokemonInfoActivity
+import com.saydullin.pokemon.app.ui.theme.TextOnPrimary
 import com.saydullin.pokemon.app.viewmodels.PokemonViewModel
 import com.saydullin.pokemon.domain.models.Pokemon
 
@@ -17,15 +21,23 @@ fun PokemonList(pokemonViewModel: PokemonViewModel) {
 
     val context = LocalContext.current
     val pokemonsList = pokemonViewModel.pokemon.value
+    val loading = pokemonViewModel.loading.value
 
     val onClickPokemon = { pokemon: Pokemon ->
-        pokemonViewModel.getPokemonInfo(pokemon.url)
         val intent = Intent(context, PokemonInfoActivity::class.java)
         intent.putExtra("pokemon_url", pokemon.url)
         context.startActivity(intent)
-        Toast.makeText(context, "Clicked ${pokemon.name}", Toast.LENGTH_SHORT).show()
     }
 
+    if (loading == true) {
+        Box(
+            modifier = Modifier.fillMaxSize().background(TextOnPrimary),
+            contentAlignment = Alignment.Center,
+        ) {
+            CircularProgressIndicator()
+        }
+        return
+    }
     LazyColumn(
         modifier = Modifier.fillMaxSize(),
     ) {
