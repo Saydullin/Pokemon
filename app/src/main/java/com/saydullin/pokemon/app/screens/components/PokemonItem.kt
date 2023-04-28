6d58a260ae3b9054a703e1066ac048ae898f167d
saydullin <saydullindev@gmail.com>
2023-04-29 02:49:18 +0300

    PokemonItem.kt moved

--- app/src/main/java/com/saydullin/pokemon/app/screens/components/PokemonItem.kt
+++ app/src/main/java/com/saydullin/pokemon/app/screens/components/PokemonItem.kt
@@ -1,4 +1,4 @@
-package com.saydullin.pokemon.app.screens
+package com.saydullin.pokemon.app.screens.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
