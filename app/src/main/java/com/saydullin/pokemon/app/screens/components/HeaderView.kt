872f8f603cc6c05e5774584fe300bd268e98c430
saydullin <saydullindev@gmail.com>
2023-04-29 02:47:47 +0300

    HeaderView.kt ui edit

--- app/src/main/java/com/saydullin/pokemon/app/screens/components/HeaderView.kt
+++ app/src/main/java/com/saydullin/pokemon/app/screens/components/HeaderView.kt
@@ -1,6 +1,6 @@
-package com.saydullin.pokemon.app.screens
+package com.saydullin.pokemon.app.screens.components
 
-import android.app.Activity
+import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -16,15 +16,20 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import com.saydullin.pokemon.R
 import com.saydullin.pokemon.app.ui.theme.Primary
 import com.saydullin.pokemon.app.ui.theme.TextOnPrimary
 import com.saydullin.pokemon.app.viewmodels.PokemonViewModel
 
 @Composable
-fun HeaderView(title: String, pokemonViewModel: PokemonViewModel, hasBackButton: Boolean = false) {
+fun HeaderView(
+    title: String,
+    navController: NavController,
+    pokemonViewModel: PokemonViewModel,
+    hasBackButton: Boolean = false
+) {
 
-    val activity = (LocalContext.current as? Activity)
     val context = LocalContext.current
     val isOffline = pokemonViewModel.isOffline.value
 
@@ -48,7 +53,7 @@ fun HeaderView(title: String, pokemonViewModel: PokemonViewModel, hasBackButton:
                             .width(25.dp)
                             .height(25.dp),
                         onClick = {
-                            activity?.finish()
+                            navController.popBackStack()
                         }
                     ) {
                         Icon(
@@ -67,24 +72,30 @@ fun HeaderView(title: String, pokemonViewModel: PokemonViewModel, hasBackButton:
                 )
             }
             if (isOffline) {
-                IconButton(
-                    modifier = Modifier
-                        .width(25.dp)
-                        .height(25.dp),
-                    onClick = {
-                        Toast.makeText(context, context.getString(R.string.no_connection), Toast.LENGTH_SHORT).show()
-                    }
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_no_connection),
-                        tint = Color.White,
-                        contentDescription = context.getString(R.string.cd_back)
-                    )
-                }
+                HeaderOfflineIcon(context)
             }
+        }
+    }
+}
+
+@Composable
+fun HeaderOfflineIcon(context: Context) {
+
+    Row() {
+        IconButton(
+            modifier = Modifier
+                .width(25.dp)
+                .height(25.dp),
+            onClick = {
+                Toast.makeText(context, context.getString(R.string.no_connection), Toast.LENGTH_SHORT).show()
+            }
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_no_connection),
+                tint = Color.White,
+                contentDescription = context.getString(R.string.cd_back)
+            )
         }
     }
 
 }
-
-
