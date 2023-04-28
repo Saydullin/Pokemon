21030a8e9dcbf2f38a929d8776b429d447990c08
saydullin <saydullindev@gmail.com>
2023-04-28 14:35:58 +0300

    MainScreen.kt background ui edit

--- app/src/main/java/com/saydullin/pokemon/app/screens/MainScreen.kt
+++ app/src/main/java/com/saydullin/pokemon/app/screens/MainScreen.kt
@@ -1,14 +1,27 @@
 package com.saydullin.pokemon.app.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.saydullin.pokemon.R
+import com.saydullin.pokemon.app.ui.theme.TextOnPrimary
 import com.saydullin.pokemon.app.viewmodels.PokemonViewModel
 
 @Composable
 fun MainScreen(pokemonViewModel: PokemonViewModel) {
 
-    Column {
-        HeaderView(pokemonViewModel)
+    val context = LocalContext.current
+    val appName = context.getString(R.string.app_name)
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(TextOnPrimary)
+    ) {
+        HeaderView(appName, pokemonViewModel)
         PokemonList(pokemonViewModel)
     }
 
