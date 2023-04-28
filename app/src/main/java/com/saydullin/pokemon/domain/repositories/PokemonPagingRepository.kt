3d9a51324d1133042780eac9c60e1d8bc48acd43
saydullin <saydullindev@gmail.com>
2023-04-29 02:35:21 +0300

    PokemonPagingRepository interface added

---
+++ app/src/main/java/com/saydullin/pokemon/domain/repositories/PokemonPagingRepository.kt
@@ -0,0 +1,13 @@
+package com.saydullin.pokemon.domain.repositories
+
+import androidx.paging.PagingData
+import com.saydullin.pokemon.data.db.entities.PokemonEntity
+import kotlinx.coroutines.flow.Flow
+
+interface PokemonPagingRepository {
+
+    fun getAllPokemons(): Flow<PagingData<PokemonEntity>>
+
+}
+
+
