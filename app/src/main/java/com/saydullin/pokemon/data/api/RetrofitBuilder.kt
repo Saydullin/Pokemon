c8621e41324a8612bd049204b6f3dde135fad3df
saydullin <saydullindev@gmail.com>
2023-04-28 21:54:50 +0300

    RetrofitBuilder.kt API url removed

--- app/src/main/java/com/saydullin/pokemon/data/api/RetrofitBuilder.kt
+++ app/src/main/java/com/saydullin/pokemon/data/api/RetrofitBuilder.kt
@@ -1,16 +1,15 @@
-package com.saydullin.pokemon.api
+package com.saydullin.pokemon.data.api
 
+import com.saydullin.pokemon.data.utils.Constants.BASE_URL
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitBuilder {
 
-    private const val baseUrl = "https://pokeapi.co/api/v2/"
-
     fun getInstance(): Retrofit {
 
-        return Retrofit.Builder().baseUrl(baseUrl)
+        return Retrofit.Builder().baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .client(OkHttpClient())
             .build()
