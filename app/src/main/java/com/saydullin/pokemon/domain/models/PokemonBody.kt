a555323faaa08488c4ecb3f654f0cc84d6b7d065
saydullin <saydullindev@gmail.com>
2023-04-29 02:50:56 +0300

    PokemonBody.kt next field set nullable

--- app/src/main/java/com/saydullin/pokemon/domain/models/PokemonBody.kt
+++ app/src/main/java/com/saydullin/pokemon/domain/models/PokemonBody.kt
@@ -4,14 +4,14 @@ import com.saydullin.pokemon.data.db.entities.PokemonBodyEntity
 
 data class PokemonBody(
     val count: Int,
-    val next: String,
+    val next: String?,
     val previous: String?,
     val results: ArrayList<Pokemon>
 ) {
 
     fun toPokemonBodyEntity() = PokemonBodyEntity(
         count = count,
-        next = next,
+        next = next ?: "",
         previous = previous ?: "",
         pokemonsList = results
     )
