74d1556d8b91a43f5fc02e17ba7c3d155a1529eb
saydullin <saydullindev@gmail.com>
2023-04-29 02:44:26 +0300

    Database daos & entities added

--- app/src/main/java/com/saydullin/pokemon/data/db/AppDatabase.kt
+++ app/src/main/java/com/saydullin/pokemon/data/db/AppDatabase.kt
@@ -7,15 +7,16 @@ import com.saydullin.pokemon.data.db.converters.PokemonListConverter
 import com.saydullin.pokemon.data.db.converters.PokemonTypeListConverter
 import com.saydullin.pokemon.data.db.dao.PokemonDao
 import com.saydullin.pokemon.data.db.dao.PokemonInfoDao
-import com.saydullin.pokemon.data.db.entities.PokemonBodyEntity
-import com.saydullin.pokemon.data.db.entities.PokemonEntity
-import com.saydullin.pokemon.data.db.entities.PokemonInfoEntity
+import com.saydullin.pokemon.data.db.dao.PokemonItemDao
+import com.saydullin.pokemon.data.db.dao.PokemonRemoteKeysDao
+import com.saydullin.pokemon.data.db.entities.*
 
 @Database(
     entities = [
         PokemonEntity::class,
         PokemonBodyEntity::class,
         PokemonInfoEntity::class,
+        PokemonRemoteKeysEntity::class,
     ],
     version = 1,
     exportSchema = true,
@@ -29,5 +30,9 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun pokemonDao(): PokemonDao
 
     abstract fun pokemonInfoDao(): PokemonInfoDao
+
+    abstract fun pokemonItemDao(): PokemonItemDao
+
+    abstract fun pokemonRemoteKeysDao(): PokemonRemoteKeysDao
 
 }
