1135957fb25abe9b5a88a73c73915d18b74241e3
saydullin <saydullindev@gmail.com>
2023-04-29 02:46:15 +0300

    Pokemon.kt mapper added

--- app/src/main/java/com/saydullin/pokemon/domain/models/Pokemon.kt
+++ app/src/main/java/com/saydullin/pokemon/domain/models/Pokemon.kt
@@ -1,9 +1,27 @@
 package com.saydullin.pokemon.domain.models
 
+import com.saydullin.pokemon.data.db.entities.PokemonEntity
+
 data class Pokemon(
     val id: Int,
     val name: String,
     val url: String,
-)
+) {
+
+    companion object {
+        val empty = Pokemon(
+            id = -1,
+            name = "",
+            url = ""
+        )
+    }
+
+    fun toPokemonEntity() = PokemonEntity(
+        id = id,
+        name = name,
+        url = url
+    )
+
+}
 
 
